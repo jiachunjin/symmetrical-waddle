b4869f4cd9183c48205a18e157dd64fb86759743
Jiachun Jin <jiachun22.jin@gmail.com>
2025-10-28 23:17:30 +0800

    s

--- runner/call_qwen_image/generate.py
+++ runner/call_qwen_image/generate.py
@@ -38,7 +38,8 @@ def generate():
     pipe = QwenImagePipeline.from_pretrained("/data/phd/jinjiachun/ckpt/Qwen/Qwen-Image", torch_dtype=dtype)
     pipe = pipe.to(accelerator.device, dtype)
 
-    print("load done!")
+    text_encoder = Qwen2_5_VLForConditionalGeneration.from_pretrained("/data/phd/jinjiachun/experiment/sft_qwenvl/gemini_flash_2047_full")
+    text_encoder = text_encoder.to(accelerator.device, dtype).eval()
 
     local_rank = accelerator.local_process_index
     # path = "data/rewritten_wise/gemini_clean.jsonl"
@@ -56,7 +57,7 @@ def generate():
 
     local_data = []
 
-    with open(f"data/rewritten_wise/after_sft/g{local_rank}.jsonl", "r") as f:
+    with open(f"/data/phd/jinjiachun/codebase/symmetrical-waddle/data/rewritten_wise/after_sft/g{local_rank}.jsonl", "r") as f:
         for line in f:
             data = json.loads(line)
             pid = int(data["prompt_id"])
@@ -74,19 +75,11 @@ def generate():
 
     # print(f"GPU {local_rank}: 处理 {len(local_data)} 个样本 (索引 {start_idx}-{end_idx-1})")
 
-    # for pid, prompt in local_data:
-    while True:
-
-        response = "The original prompt is a good starting point, but it could be more descriptive to guide an image generation model effectively. Here's my reasoning for optimization:  *   **\"Unfinished basilica\"**: This is a bit vague. How unfinished? What does that look like visually? Is it just a shell, or are there details present? *   **\"Masterpiece of Catalan Modernism\"**: While this sets a style, it doesn't describe *what* that looks like. We need to break down the visual characteristics of Catalan Modernism. *   **\"Intricate facades\"**: Again, \"intricate\" is good, but we can be more specific about *how* they are intricate.  My goal is to add visual detail and clarify the state of the basilica to make it easier for the AI to render a compelling image.  Revised Prompt: {A grand, partially completed basilica, showcasing the distinctive architectural style of Catalan Modernism. Its exterior facades are adorned with elaborate, organic, and flowing patterns, featuring a rich palette of warm colors like terracotta, ochre, and burnt orange. The stone work is detailed with sculpted figures and geometric shapes, creating a sense of dynamic movement across the surfaces. Some areas of the structure are still bare or under construction, revealing rough-hewn stone and incomplete arches, while others display fully realized, ornate decorations. The overall impression is one of a magnificent, unfinished work of art, bathed in natural light.}"
-
-        prompt = response.split("{")[1].split("}")[0]
-        print(prompt)
-
+    for pid, prompt in local_data:
         prompt_neg = [" "]
-        # print(f"GPU {local_rank}: {pid} - {prompt}")
-        print(local_rank, prompt)
+        print(f"GPU {local_rank}: {pid} - {prompt}")
 
-        prompt_embeds, prompt_embeds_mask = encode([prompt], pipe.text_encoder)
+        prompt_embeds, prompt_embeds_mask = encode([prompt], text_encoder)
         prompt_embeds_neg, prompt_embeds_mask_neg = pipe._get_qwen_prompt_embeds(
             prompt                = prompt_neg,
             device                = accelerator.device,
@@ -103,10 +96,9 @@ def generate():
             width                       = 512,
         ).images[0]
 
-        save_name = f"/data/phd/jinjiachun/codebase/{local_rank}.png"
+        save_name = f"/data/phd/jinjiachun/codebase/qimagined-goggles/asset/gemini_flash_2047_full_oneencoder/{pid}.png"
 
         image.save(save_name)
-        exit(0)
 
 
 if __name__ == "__main__":
