1b58edc0d051e6916f07b29e4911b837f8880add
Jiachun Jin <jiachun22.jin@gmail.com>
2025-10-30 12:44:43 +0800

    s

--- runner/call_qwen_image/generate.py
+++ runner/call_qwen_image/generate.py
@@ -61,7 +61,6 @@ def generate():
             # 把被大括号包起来的内容作为prompt
             prompt = response.split("Revised Prompt")[-1].strip()
             print(pid, prompt)
-    exit(0)
     #         local_data.append((pid, prompt))
 
     # chunk_size = (len(all_data) + num_processes - 1) // num_processes
